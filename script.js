96319562f6920d2dc6e8c17db25400d9623d58ae
Verónica Vidal Polanía <vidalveropolania@gmail.com>
2025-02-18 09:54:15 -0500

    Ejercicio #1 #2

---
+++ script.js
@@ -0,0 +1,15 @@
+
+//Ejercicio 1
+let nombre = "Verónica";
+let edad = 24;
+let ciudad = "Buenos Aires";
+console.log (" Mi nombre es " + nombre + ", tengo " + edad + " años y vivo en " + ciudad); 
+/* CREA UNA VARIABLE LLAMADA PRECIO Y ASIGNALE UN VALOR DECIMAL. LUEGO
+CREA OTRA VARIABLE LLAMADA CANTIDAD Y ASIGNALE UN VALOR ENTERO.
+MUESTRE EL TIPO DE DATOS DE CADA VARIABLE EN LA CONSOLA.
+*/
+let precio = 3.14;
+let cantidad = 5;
+console.log (typeof cantidad);//imprime en la consola
+document.write(typeof cantidad);//imprime en el navegador
+alert(typeof cantidad);//imprime en una giventana emergente
