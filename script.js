267a642ca842c9a38de61a94688b09bc15a299df
Verónica Vidal Polanía <vidalveropolania@gmail.com>
2025-02-18 11:33:32 -0500

    se agregaron otros ejercicios

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ let cantidad = 5;
 console.log (typeof cantidad);//imprime en la consola
 document.write(typeof cantidad);//imprime en el navegador
 alert(typeof cantidad);//imprime en una giventana emergente
+
+//Ejercicio 1 ejercicios en javascript
+let producto = 2000;
+let descuento = 0.25;
+let precioFinal = producto * descuento;
+console.log(precioFinal);
+
+//Ejercicio 2 ejercicios en javascript
+let capital = 500000;
+let aumento = 0.02;
+let gananciafinal = capital * aumento;
+console.log(gananciafinal);
+
+//Ejercicio 3 ejercicios en javascript
+let calificaciones_parciales = 0.55;
+let examen_final = 0.30;
+let trabajo_final = 0.15;
+let nota_final = calificaciones_parciales + examen_final + trabajo_final;
+console.log(nota_final);
+
+//Ejercicio 4 ejercicios en javascript
+let articulo = 3000;
+let ganancia = 0.3;
+let precio_final = articulo * ganancia;
+console.log(precio_final);
+
+//Ejercicio 5 ejercicios en javascript
+let kilometros = 10
+let minutos = 6
